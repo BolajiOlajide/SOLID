9b484d57c4c390e5392222ef0d2531015903abfe
Bolaji Olajide <bolaji.olajide@andela.com>
2018-07-03 21:12:47 +0100

    correct code references for the following:
    - single responsibility
    - open-closed principle

--- S/bad.js
+++ S/bad.js
@@ -1,7 +1,3 @@
-// SINGLE Responsibility
-
-// BAD
-
 const shape = (length, type) => {
   const proto = {
     type,
@@ -15,32 +11,3 @@ const shape = (length, type) => {
 
   return Object.assign(Object.create(proto), { length, type });
 };
-
-const areaCalculator = (shapes) => {
-  const proto = {
-    sum() {
-      const area = [];
-      for (_ of this.shapes) {
-        area.push(_.area())
-      }
-      return area.reduce((v, c) => c += v, 0);
-    },
-    output () {
-     return `Sum of the areas of provided shapes: ${this.sum()}`
-    }
-  };
-
-  return Object.assign(Object.create(proto), { shapes });
-};
-
-const shapes = [
-  shape(2, 'Circle'),
-  shape(5, 'Square'),
-  shape(6, 'Square')
-];
-
-const areas = areaCalculator(shapes);
-console.log(areas.output());
-
-// When I have a new shape to add it will be stressful as I have to add another
-// condition to the if-else statement contained in the shapes method.
--- S/good.js
+++ S/good.js
@@ -1,49 +1,20 @@
-// SINGLE Responsibility
+// SINGLE RESPONSIBILITY
 
-// GOOD
+// PRINCIPLE:
+// A class should have one and only one reason to change, meaning that a class should only have one job.
+
 const circle = (radius) => {
   const proto = {
     type: 'Circle',
-    area() {
-      return Math.PI * Math.pow(this.radius, 2);
-    }
-  };
+    //code
+  }
+  return Object.assign(Object.create(proto), {radius})
+};
 
-  return Object.assign(Object.create(proto), { radius });
-}
 const square = (length) => {
   const proto = {
     type: 'Square',
-    area () { return Math.pow(this.length, 2); }
-  };
-
-  return Object.assign(Object.create(proto), { length });
+    //code
+  }
+  return Object.assign(Object.create(proto), {length})
 };
-
-const areaCalculator = (shapes) => {
-  const proto = {
-    sum() {
-      const area = [];
-      for (_ of this.shapes) {
-        area.push(_.area())
-      }
-      return area.reduce((v, c) => c += v, 0);
-    },
-    output () {
-     return `Sum of the areas of provided shapes: ${this.sum()}`
-    }
-  };
-
-  return Object.assign(Object.create(proto), { shapes });
-};
-
-const shapes = [
-  circle(2),
-  square(5),
-  square(6)
-];
-
-const areas = areaCalculator(shapes);
-console.log(areas.output());
-
-// When I have a new shape I can simply create a new factory function for it.
